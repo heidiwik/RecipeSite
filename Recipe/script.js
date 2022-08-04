52017f34b49850ef97351c98d5dfba030a63a516
Heidi Wikman <heidi.wikman@kapsi.fi>
2022-08-04 22:23:23 +0300

    Recipe site

---
+++ Recipe/script.js
@@ -0,0 +1,31 @@
+
+const activateSettings = () => {
+
+    const config = {
+        "title" : "My recipes",
+        "database" : "localhost",
+        "database_pass" : "B40G12#8dP1N",
+        "public" : true,
+        "settings" : [
+            {
+                "name": "text-size",
+                "value": "26",
+                "version": 2.0
+            },
+            {
+                "name": "background-color",
+                "value": "#f5f5dc",
+                "version": 1.5
+            },
+            {
+                "name": "display-news",
+                "value": 5,
+                "version": 0.1
+            }
+        ]
+      };
+}
+
+
+
+
